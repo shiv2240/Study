835a888b9c07750e2b0f9dcc0bee3edd85bd6f78
Shiv Sahni <shivsahni2240@gmail.com>
2025-02-28 02:02:11 +0530

    day-3 practise

--- DevTinder/DEVTINDER-BE/src/app.js
+++ DevTinder/DEVTINDER-BE/src/app.js
@@ -8,6 +8,29 @@ app.get("/",(req,res)=>{
     res.send("Homepage");
 });
 
+app.get("/user/:userId/:password",(req,res)=>{
+    console.log(req.params)
+    res.send([{
+        firstName:"Shiv",
+        lastName:"Sahni"
+    },{
+        firstName:"Shubham",
+        lastName:"Kumar"
+    }])
+})
+
+app.get("/ab?c",(req,res)=>{
+    res.send("You are on ab and ac")
+})
+
+app.post("/user",(req,res)=>{
+    res.send("Data Saved Successfully")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted successfully")
+})
+
 app.get("/hello",(req,res)=>{
     res.send("Hello AEEEE haalo!");
 });
