bf54a2cc73f821efa2638a7ecb5ab9c9c5d9804b
Shiv Sahni <shivsahni2240@gmail.com>
2025-03-08 00:21:22 +0530

    TImer

--- React/Timer/src/components/timer.jsx
+++ React/Timer/src/components/timer.jsx
@@ -1,33 +1,37 @@
 import { useRef, useState } from "react";
 
-function Timer () {
-    const [time, setTime] = useState(0);
-    const isTime = useRef(null);
+function Timer() {
+  const [time, setTime] = useState(0);
+  const isTime = useRef(null);
 
-    function handleStart(){
-        isTime.current = setInterval(()=>{
-            setTime((time) => time+1);
-        },1000)
+  function handleStart() {
+    if (!isTime.current) {
+      isTime.current = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
     }
+  }
 
-    function handleStop(){
-        clearInterval(isTime.current);
-    }
+  function handleStop() {
+    clearInterval(isTime.current);
+    isTime.current = null;
+  }
 
-    function handleReset(){
-        clearInterval(isTime.current);
-        setTime(0);
-    }
+  function handleReset() {
+    clearInterval(isTime.current);
+    isTime.current = null;
+    setTime(0);
+  }
 
-    return (
-        <>
-            <h1>Timer</h1>
-            <h2>{time}</h2>
-            <button onClick={handleStart}>Start</button>
-            <button onClick={handleStop}>Stop</button>
-            <button onClick={handleReset}>Reset</button>
-        </>
-    )
+  return (
+    <>
+      <h1>Timer</h1>
+      <h2>{time}</h2>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+    </>
+  );
 }
 
 export default Timer;
